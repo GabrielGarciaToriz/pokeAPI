57377de0abd6c2c490f65bdc09ae9e9f01ece82b
Digis 01 <digis01Sistemas@outlook.com>
2026-04-23 14:16:12 -0600

    Component, interface y servicio

---
+++ src/app/pokemon-main-component/pokemon-main-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-main-component',
+  imports: [],
+  templateUrl: './pokemon-main-component.html',
+  styleUrl: './pokemon-main-component.css',
+})
+export class PokemonMainComponent {
+
+}
