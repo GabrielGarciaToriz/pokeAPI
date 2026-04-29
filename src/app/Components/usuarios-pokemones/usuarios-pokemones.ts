a549fb6823c5c45e57a7948333b00fc96ecab220
dilanaraizac18 <dilanaraiza18@outlook.com>
2026-04-28 19:11:33 -0600

    new getById

---
+++ src/app/Components/usuarios-pokemones/usuarios-pokemones.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios-pokemones',
+  imports: [],
+  templateUrl: './usuarios-pokemones.html',
+  styleUrl: './usuarios-pokemones.css',
+})
+export class UsuariosPokemones {}
